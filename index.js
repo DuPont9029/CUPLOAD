fa3a5f7f844505c39f3a56f51efc418df861af15
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2023-10-22 20:34:21 +0200

    refactor: :closed_lock_with_key:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import fs from 'fs'; // Import the file system module
 import { createRequire } from 'module'; // Bring in the ability to create the 'require' method
 const require = createRequire(import.meta.url); // construct the require method
-require('dotenv').config({ path: './key.env' }); // use the require method
+require('dotenv').config(); // use the require method
 
 const s3_client = new S3Client({
     endpoint: "https://s3.cubbit.eu",
